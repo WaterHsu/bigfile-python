726ee9c675562103562a99b36db8db9aed0f0071
WaterHsu <hsupangfei1989@gmail.com>
2014-09-11 00:53:49 +0800

    加入缓存的读取  速度快了很多

--- bigfile-python/src/bigfile/deal/FileReader.py
+++ bigfile-python/src/bigfile/deal/FileReader.py
@@ -1,14 +1,27 @@
 import time
 
+
 def read():
-    start = time.time();
-    fileName = "E://access.29.log"
+    start = time.time()
+    fileName = "E://access-demo.log"
     with open(fileName, "r") as f:
         for line in f:
-            m = 1;
+            m = 1
             
     end = time.time()
     print end - start 
     
+def read2():
+    start = time.time()
+    fileName = "E://access-demo.log"
+    with open(fileName, "r") as f:
+        lines = f.readlines(100000)
+        
+        for line in lines:
+            m = 1
+            
+    end = time.time()
+    print end - start
+    
 if __name__ == '__main__':
-    read()
+    read2()
